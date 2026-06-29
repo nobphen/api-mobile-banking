aaf43ce8a4ee4338ffd8087773fb8030c084858c
nobphen <nobphen11@gmail.com>
2026-06-29 17:07:33 +0700

    create delete find by nane done

--- src/main/java/com/phen/mbanking/features/accounttype/AccountTypeController.java
+++ src/main/java/com/phen/mbanking/features/accounttype/AccountTypeController.java
@@ -1,6 +1,7 @@
 package com.phen.mbanking.features.accounttype;
 
 
+import com.phen.mbanking.features.account.dto.AccountResponse;
 import com.phen.mbanking.features.accounttype.dto.AccountTypeRequest;
 import com.phen.mbanking.features.accounttype.dto.AccountTypeResponse;
 import jakarta.validation.Valid;
@@ -30,5 +31,50 @@ public class AccountTypeController {
         return accountTypeService.creatAccountType(accountTypeRequest);
     }
 
+
+    /**
+     * Find all account type
+     *
+     * @return {@link  List<AccountTypeResponse>}
+     */
+    @GetMapping
+    List<AccountTypeResponse> findAll() {
+        return accountTypeService.findAll();
+    }
+
+
+    /**
+     * Find account type by name
+     *
+     * @param name of account type
+     * @return {@link AccountTypeResponse }
+     */
+    @GetMapping("/{name}")
+    AccountTypeResponse findByName(@PathVariable String name) {
+        return accountTypeService.findByName(name);
+    }
+
+
+    /**
+     * Update account type
+     *
+     * @param id                 of account type id
+     * @param accountTypeRequest {@link AccountTypeRequest}
+     * @return {@link AccountTypeResponse}
+     */
+    @PutMapping("/{id}")
+    AccountTypeResponse updateAccountType(@Valid @PathVariable Integer id, AccountTypeRequest accountTypeRequest) {
+        return accountTypeService.updateAccountType(id, accountTypeRequest);
+    }
+
+
+    /**
+     * Delete Account Type (Soft Delete)
+     */
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteAccountType(@PathVariable Integer id) {
+        accountTypeService.delectAccountType(id);
+    }
 
 }
--- src/main/java/com/phen/mbanking/features/accounttype/AccountTypeServiceImp.java
+++ src/main/java/com/phen/mbanking/features/accounttype/AccountTypeServiceImp.java
@@ -44,23 +44,70 @@ public class AccountTypeServiceImp implements AccountTypeService {
 
     @Override
     public List<AccountTypeResponse> findAll() {
-       return  null;
+       return  accountTypeRepository.findAllByIsDeletedFalse().stream().map(accountTypeMapper::toAccountTypeResponse).toList();
     }
 
     @Override
     public AccountTypeResponse findByName(String name) {
-      return  null;
+
+        AccountType accountType = accountTypeRepository.findByNameAndIsDeletedFalse(name).orElseThrow(
+                () -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "Account Type not found."
+                )
+        );
+
+
+      return  accountTypeMapper.toAccountTypeResponse(accountType);
     }
 
     @Override
     public AccountTypeResponse updateAccountType(Integer id, AccountTypeRequest accountTypeRequest) {
 
+        AccountType accountType = accountTypeRepository.findByIdAndIsDeletedFalse(id).orElseThrow(
+                () -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "Account Type id not found."
+                )
+        );
+
+        // Validate alias
+        if(!accountType.getAlias().equals(accountTypeRequest.alias())){
+            validateAlias(accountTypeRequest.alias());
+        }
+
+        // Validate name
+        if(!accountType.getName().equals(accountTypeRequest.name())){
+            validateName(accountTypeRequest.name());
+        }
 
 
+        accountType.setAlias(accountTypeRequest.alias());
+        accountType.setName(accountTypeRequest.name());
+        accountType.setDescription(accountTypeRequest.description());
+
+        /// Save data and get back data
+        accountType = accountTypeRepository.save(accountType);
+
+
+        return accountTypeMapper.toAccountTypeResponse(accountType);
     }
 
     @Override
     public void delectAccountType(Integer id) {
+
+        AccountType accountType = accountTypeRepository.findByIdAndIsDeletedFalse(id).orElseThrow(
+                () -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "Account Type id not found."
+                )
+        );
+
+
+        // Set update data
+        accountType.setIsDeleted(true);
+
+        accountTypeRepository.save(accountType);
 
     }
 
