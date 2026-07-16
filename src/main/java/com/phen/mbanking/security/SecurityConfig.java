1469c8e98b9f8525203e5f2c3363c098dd9e5de6
nobphen <nobphen11@gmail.com>
2026-07-16 15:48:44 +0700

    add spring security

---
+++ src/main/java/com/phen/mbanking/security/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.phen.mbanking.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+//    @Bean
+//    SecurityFilterChain configureApiSecurity(HttpSecurity http){
+//
+//    }
+}
