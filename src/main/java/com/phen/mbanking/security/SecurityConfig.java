d58bfdc80d4c6b32d9357cab79118a1df4422917
MOBILE-APP.03 <MOBILE-APP.03@TURBOTECH.COM>
2026-07-20 11:51:10 +0700

    make security config

--- src/main/java/com/phen/mbanking/security/SecurityConfig.java
+++ src/main/java/com/phen/mbanking/security/SecurityConfig.java
@@ -2,14 +2,34 @@ package com.phen.mbanking.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-//    @Bean
-//    SecurityFilterChain configureApiSecurity(HttpSecurity http){
-//
-//    }
+    @Bean
+    SecurityFilterChain configureApiSecurity(HttpSecurity http) {
+
+        /// Endpoint Security Config
+        http.authorizeHttpRequests(endpoint -> endpoint.anyRequest().authenticated());
+
+
+        /// Security Mechanism ( HTTP Basic Auth )
+        /// HTTP Basic Auth ( Username & Password)
+        http.httpBasic(Customizer.withDefaults());
+
+
+        /// Disable CSRF Token
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        /// Make Stateless Session
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+
+    }
 }
