23227fc2ee778d825176a279fe99e26dbaead2bb
MOBILE-APP.03 <MOBILE-APP.03@TURBOTECH.COM>
2026-07-23 11:24:39 +0700

    add Inmemory user

--- src/main/java/com/phen/mbanking/security/SecurityConfig.java
+++ src/main/java/com/phen/mbanking/security/SecurityConfig.java
@@ -6,10 +6,52 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    InMemoryUserDetailsManager configureUseSecurity(){
+
+        // User admin
+        UserDetails admin = User
+                .withUsername("admin")
+                .password("{noop}admin123")
+                .roles("USER","ADMIN")
+                .build();
+
+
+        /// User editor
+        UserDetails editor = User
+                .withUsername("editor")
+                .password("{noop}editor123")
+                .roles("USER","EDITOR")
+                .build();
+
+        /// User subscriber
+        UserDetails subscriber = User
+                .withUsername("subscriber")
+                .password("{noop}subscriber123")
+                .roles("USER","'SUBSCRIBER'")
+                .build();
+
+        /// Manger memory
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        manager.createUser(admin);
+        manager.createUser(editor);
+        manager.createUser(subscriber);
+
+
+        return  manager;
+
+
+    }
 
     @Bean
     SecurityFilterChain configureApiSecurity(HttpSecurity http) {
