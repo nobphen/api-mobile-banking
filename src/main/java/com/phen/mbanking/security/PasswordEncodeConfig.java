19c1c2b1c62ccd662bf97ff8738839808fd601ae
MOBILE-APP.03 <MOBILE-APP.03@TURBOTECH.COM>
2026-07-28 13:21:00 +0700

    set role permission to user

---
+++ src/main/java/com/phen/mbanking/security/PasswordEncodeConfig.java
@@ -0,0 +1,15 @@
+package com.phen.mbanking.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordEncodeConfig {
+
+    @Bean
+    PasswordEncoder configPasswordEncode (){
+        return  new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/com/phen/mbanking/security/SecurityConfig.java
+++ src/main/java/com/phen/mbanking/security/SecurityConfig.java
@@ -1,43 +1,65 @@
 package com.phen.mbanking.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final PasswordEncoder passwordEncoder;
+
+
     @Bean
-    InMemoryUserDetailsManager configureUseSecurity(){
+    InMemoryUserDetailsManager configureUseSecurity() {
+
 
         // User admin
         UserDetails admin = User
                 .withUsername("admin")
-                .password("{noop}admin123")
-                .roles("USER","ADMIN")
+                .password(passwordEncoder.encode("admin123"))
+                .roles("USER", "ADMIN")
                 .build();
 
 
         /// User editor
         UserDetails editor = User
                 .withUsername("editor")
-                .password("{noop}editor123")
-                .roles("USER","EDITOR")
+                .password(passwordEncoder.encode("editor123"))
+                .roles("USER", "EDITOR")
                 .build();
 
         /// User subscriber
         UserDetails subscriber = User
                 .withUsername("subscriber")
-                .password("{noop}subscriber123")
-                .roles("USER","'SUBSCRIBER'")
+                .password(passwordEncoder.encode("subscriber123"))
+                .roles("USER", "'SUBSCRIBER'")
+                .build();
+
+        /// User customer
+        UserDetails customer = User
+                .withUsername("customer")
+                .password(passwordEncoder.encode("customer123"))
+                .roles("USER", "CUSTOMER")
+                .build();
+
+        /// User customer
+        UserDetails managers = User
+                .withUsername("manager")
+                .password(passwordEncoder.encode("manager123"))
+                .roles("USER", "MANAGER")
                 .build();
 
         /// Manger memory
@@ -46,9 +68,10 @@ public class SecurityConfig {
         manager.createUser(admin);
         manager.createUser(editor);
         manager.createUser(subscriber);
+        manager.createUser(customer);
+        manager.createUser(managers);
 
-
-        return  manager;
+        return manager;
 
 
     }
@@ -57,7 +80,20 @@ public class SecurityConfig {
     SecurityFilterChain configureApiSecurity(HttpSecurity http) {
 
         /// Endpoint Security Config
-        http.authorizeHttpRequests(endpoint -> endpoint.anyRequest().authenticated());
+        http.authorizeHttpRequests(endpoint -> endpoint
+                .requestMatchers(HttpMethod.POST, "/v1/api/accounts/**").hasAnyRole("USER")
+                .requestMatchers(HttpMethod.GET, "/api/v1/accounts/**").hasAnyRole("USER")
+                .requestMatchers(HttpMethod.PUT, "/api/v1/accounts/**").hasAnyRole("USER")
+                .requestMatchers(HttpMethod.PATCH, "/api/v1/accounts/**").hasAnyRole("USER")
+                .requestMatchers(HttpMethod.DELETE, "/api/v1/accounts/**").hasAnyRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/api/v1/account-types/**").hasAnyRole("MANAGER", "ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/api/v1/account-types/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/api/v1/account-types/**").hasAnyRole("USER")
+                .requestMatchers(HttpMethod.PUT, "/api/v1/account-types/**").hasAnyRole("MANAGER", "ADMIN")
+                .requestMatchers(HttpMethod.PATCH, "/api/v1/account-types/**").hasAnyRole("MANAGER", "ADMIN")
+                .anyRequest()
+                .authenticated()
+        );
 
 
         /// Security Mechanism ( HTTP Basic Auth )
